333ab5c08ef3db36c7d0b20a99cd6b37b617c87f
Leonardo Tricotti <tricottileo@gmail.com>
2026-03-13 09:53:03 -0300

    corregido index a minusculas

--- data/dataIndex.js
+++ data/dataIndex.js
@@ -1,21 +1,21 @@
 // data.js — Datos de la institución
 
 const DATA = {
-  nombre: "EP N°17 «Luis Castells»",
-  subtitulo: "Sistema de evaluación docente",
-  directora: "Directora: Quinteros Vanesa",
-  version: "v2.0 — 2025",
+  nombre: 'EP N°17 «Luis Castells»',
+  subtitulo: 'Sistema de evaluación docente',
+  directora: 'Directora: Quinteros Vanesa',
+  version: 'v2.0 — 2025',
 
   menu: [
     {
-      texto: "📋 Rúbrica de Evaluación Docente",
-      href: "Evaluacion.html",
-      claseExtra: "btn-eval"
+      texto: '📋 Rúbrica de Evaluación Docente',
+      href: 'evaluacion.html',
+      claseExtra: 'btn-eval',
     },
     {
-      texto: "📂 Monitoreo de Carpeta Didáctica",
-      href: "Monitoreo.html",
-      claseExtra: "btn-mon"
-    }
-  ]
+      texto: '📂 Monitoreo de Carpeta Didáctica',
+      href: 'monitoreo.html',
+      claseExtra: 'btn-mon',
+    },
+  ],
 };
